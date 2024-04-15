c8e17d8b2d0e50cfb82896e27a21d7fd4a9dbdf9
flo_flooff <floflo.off@gmail.com>
2024-04-15 11:18:10 +0200

    Log des messages supprimés

---
+++ events/messageDelete.js
@@ -0,0 +1,22 @@
+const { EmbedBuilder, Collection, Message } = require("discord.js");
+const ms = require("ms");
+const client = require("..");
+const config = require("../config.json");
+
+client.on("messageDelete", async (message) => {
+  if (message.channel.id === config.channels.text.logs) return;
+  if (message.pinned) return;
+  // Récupère le message supprimé et l'envoi dans les logs
+  const embed = new EmbedBuilder()
+    .setTitle("Message supprimé")
+    .setDescription(
+      `**Auteur :** ${message.author}
+      \n**Contenu :** ${message.content} 
+      \n**Salon :** ${message.channel}`
+    )
+    .setColor("Red")
+    .setTimestamp();
+
+  const channel = client.channels.cache.get(config.channels.text.logs);
+  channel.send({ embeds: [embed] });
+});
